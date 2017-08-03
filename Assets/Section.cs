4f005605999f5625d9264d531ff18c96945b02ee
tkchu <tkchushbm@gmail.com>
2017-08-03 14:52:17 +0800

    进门开始

---
+++ Assets/Section.cs
@@ -0,0 +1,31 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Section : MonoBehaviour {
+    Transform player;
+    float gridSize;
+
+    private void Start() {
+        player = FindObjectOfType<Player>().transform;
+        gridSize = FindObjectOfType<GridWorld>().gridSize;
+    }
+
+    void Update(){
+        if(!begin && Mathf.Abs(transform.position.x - player.position.x) < gridSize/2 && Mathf.Abs(transform.position.y - player.position.y) < gridSize / 2) {
+            Begin();
+        }
+    }
+
+    public bool begin = false;
+    public void Begin() {
+        Debug.Log("let's fucking begin");
+        foreach (Transform t in transform) {
+            if (t.GetComponent<MoveEnemy>()) {
+                t.GetComponent<MoveEnemy>().willAction = true;
+            }
+        }
+        begin = true;
+    }
+
+}
