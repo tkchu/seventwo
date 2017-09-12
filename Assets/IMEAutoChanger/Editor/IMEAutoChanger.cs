8aba5e376e1145a8662536d042a9f93c6d4afacd
张巍 <1353587086@qq.com>
2017-09-12 21:29:29 +0800

    修复了之前提交的动画关联丢失的bug，添加了Unity中文输入法补丁

---
+++ Assets/IMEAutoChanger/Editor/IMEAutoChanger.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Reflection;
+
+[InitializeOnLoad]
+public class IMEAutoChanger
+{
+	private static FieldInfo s_ActiveEditorField;
+	private static FieldInfo s_TextEditorHasFocusField;
+
+	static IMEAutoChanger()
+	{
+		System.Type textEditorType = typeof(TextEditor);
+		s_TextEditorHasFocusField = textEditorType.GetField( "m_HasFocus",BindingFlags.NonPublic | BindingFlags.Instance );
+		
+		System.Type editorGUIType = typeof(EditorGUI);
+		s_ActiveEditorField = editorGUIType.GetField ( "activeEditor",BindingFlags.NonPublic | BindingFlags.Static );
+
+		EditorApplication.update += Update;
+	}
+
+	private static TextEditor activeEditor
+	{
+		get
+		{
+			return (TextEditor)s_ActiveEditorField.GetValue(null);
+		}
+	}
+	
+	private static bool HasFocus( TextEditor textEditor )
+	{
+		return textEditor != null && GUIUtility.keyboardControl != 0 && GUIUtility.keyboardControl == textEditor.controlID && (bool)s_TextEditorHasFocusField.GetValue( textEditor );
+	}
+
+	static bool m_Changed = false;
+	static IMECompositionMode m_CacheCompositionMode;
+
+	static void Update()
+	{
+		if( EditorGUIUtility.editingTextField || HasFocus( activeEditor ) )
+		{
+			if (!m_Changed)
+			{
+				m_Changed = true;
+				m_CacheCompositionMode = Input.imeCompositionMode;
+				Input.imeCompositionMode = IMECompositionMode.On;
+			}
+		}
+		else if( m_Changed )
+		{
+			m_Changed = false;
+			Input.imeCompositionMode = m_CacheCompositionMode;
+		}
+	}
+}
---
+++ Assets/Script/weaponshow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponshow : MonoBehaviour {
+    Animator animator;
+    GameObject hero;
+    Vector3 offset1,offset2,position;
+    // Use this for initialization
+    void Start () {
+       animator = this.GetComponent<Animator>();
+       hero = GameObject.Find("Hero");
+        offset1 = this.GetComponent<Transform>().position - hero.GetComponent<Transform>().position;
+        offset2 = new Vector3 (this.GetComponent<Transform>().position.x - hero.GetComponent<Transform>().position.x,hero.GetComponent<Transform>().position.y-this.GetComponent<Transform>().position.y, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        animator.SetInteger("stat", hero.GetComponent<Animator>().GetInteger("stat"));
+        if(hero.GetComponent<Animator>().GetBool("isShoot"))
+        animator.SetBool("isShoot",true);
+        if (!hero.GetComponent<SpriteRenderer>().flipX) { this.GetComponent<Transform>().position = hero.GetComponent<Transform>().position + offset1; this.GetComponent<SpriteRenderer>().flipX = false; }
+        else { this.GetComponent<Transform>().position = hero.GetComponent<Transform>().position - offset2; this.GetComponent<SpriteRenderer>().flipX = true; }
+    }
+}
+    
