5bc6741c132222ad2510cf4f73cd6dd0c5103ff9
张巍 <1353587086@qq.com>
2017-09-17 12:45:27 +0800

    现在开始界面到游戏的bgm不会重新播放了

---
+++ Assets/Script/DontDestroy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    static bool _once=true;
+	// Use this for initialization
+	void Start () {
+	    if (_once)
+	    {
+
+	        DontDestroyOnLoad(gameObject);
+	        _once = false;
+	    }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
