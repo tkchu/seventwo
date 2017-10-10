c783bceec4ce888f6bd505e7cc38e931de5f1d68
CloudLunar(PC) <cloudlunar@qq.com>
2017-10-10 19:31:33 +0800

    增加了眼睛的特效

---
+++ Assets/Eyes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour {
+    Animator ani;
+    float a=0,b;
+
+    // Use this for initialization
+    void Start () {
+        ani = this.GetComponent<Animator>();
+        b = Random.Range(5, 12);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!ani.GetBool("bool"))
+        {
+         a += Time.unscaledDeltaTime;
+            if (a > b)
+            {
+                a = 0;
+                b = Random.Range(5f, 12f);
+                ani.SetBool("bool", true);
+            }
+        }
+
+        Debug.Log(a.ToString()+" "+b.ToString());
+    }
+}
