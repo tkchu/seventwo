38533550ab83582dfd732dda09c13b86365cb709
tkchu <tkchushbm@gmail.com>
2017-11-18 20:29:21 +0800

    简单地介绍一下死因

---
+++ Assets/DieImage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DieImage : MonoBehaviour {
+    public Image playerImage;
+    public Text text;
+    public Image enemyImage;
+    private float secondsBefore = 1;
+    private float secondsContinue = 1;
+
+    public void Show(Sprite enemySprite) {
+        StartCoroutine(ShowC(enemySprite));
+    }
+    IEnumerator ShowC(Sprite enemySprite) {
+        yield return new WaitForSeconds(secondsBefore);
+        GetComponent<Image>().enabled = true;
+        playerImage.enabled = true;
+        text.enabled = true;
+        enemyImage.enabled = true;
+        enemyImage.sprite = enemySprite;
+        yield return new WaitForSeconds(secondsContinue);
+        GetComponent<Image>().enabled = false;
+        playerImage.enabled = false;
+        text.enabled = false;
+        enemyImage.enabled = false;
+    }
+}
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -140,6 +140,7 @@ public class Player : MonoBehaviour {
         {
             GetComponent<Animator>().SetBool("isDead", true);
             //item.GetComponent<Animator>().SetBool("isAttacking", true);
+            FindObjectOfType<DieImage>().Show(item.GetComponent<SpriteRenderer>().sprite);
             StartCoroutine(Restart());
         }
 
@@ -159,9 +160,9 @@ public class Player : MonoBehaviour {
     IEnumerator Restart()
     {
         isDead = true;
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1f);
 
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(1.5f);
         isDead = false;
 
         if (SceneManager.GetActiveScene().name == "boss") {
